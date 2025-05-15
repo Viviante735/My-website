d4d57aeb66fe5fd45573ed4b5bdbe7e48930e96c
DIFoundation <adeniranibrahim165@gmail.com>
2025-05-16 00:42:34 +0100

    Make Landing Page responsive

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const menuToggle = document.getElementById('menuToggle');
-const navMenu = document.getElementById('navMenu');
+const navMenu = document.getElementById('navMenuMobile');
 
 menuToggle.addEventListener('click', () => {
   navMenu.classList.toggle('show');
